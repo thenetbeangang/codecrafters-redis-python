825fcf53dc5de524bb498b2467fba344a756e6e1
thenetbeangang <Caorey13@hotmail.com>
2024-06-05 08:37:18 +0200

    Solution1

--- app/main.py
+++ app/main.py
@@ -7,7 +7,8 @@ def main():
 
     
     server_socket = socket.create_server(("localhost", 6379))
-    server_socket.accept() # wait for client
+    client, addr =    server_socket.accept() # wait for client
+    client.send(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
