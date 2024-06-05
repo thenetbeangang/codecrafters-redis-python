3d914098ea7f78bbeacdc80d6696226d16865160
thenetbeangang <Caorey13@hotmail.com>
2024-06-05 08:32:36 +0200

    Test

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ def main():
     print("Logs from your program will appear here!")
 
     
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    server_socket = socket.create_server(("localhost", 6379))
     server_socket.accept() # wait for client
 
 
