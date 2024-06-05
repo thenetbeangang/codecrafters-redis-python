cc5b5ef5ba88d0d0a6b6bc823c8152cff0b04887
thenetbeangang <Caorey13@hotmail.com>
2024-06-05 08:48:16 +0200

    asas

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ import socket
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
-
+    "Test comment"
     
     server_socket = socket.create_server(("localhost", 6379))
     client, addr =    server_socket.accept() # wait for client
